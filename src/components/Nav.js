14f2d869d7faffa5141a09e6d6b2515bc03e8a42
Ricardo Mora <ricardomora@MacBook-Pro-de-Ricardo.local>
2026-05-25 21:34:41 -0600

    Add booking form with controlled inputs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/reservations">Reservations</Link></li>
+                <li><Link to="/booking">Reservations</Link></li>
                 <li><Link to="/order-online">Order Online</Link></li>
                 <li><Link to="/login">Login</Link></li>
             </ul>
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,8 +1,11 @@
+import BookingForm from "../components/BookingForm";
+
 function BookingPage() {
     return (
-        <section>
+        <section className="booking-page">
             <h1>Book a Table</h1>
             <p>Here you can make a reservation at Little Lemon.</p>
+            <BookingForm />
         </section>
     );
 }
